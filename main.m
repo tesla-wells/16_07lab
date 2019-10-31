8932d9f69b0261fba4fdc87dff7ec48818f90016
Tesla Wells <teslaw@mac-56-129-13.mit.edu>
2019-10-31 00:49:45 -0400

    Main

---
+++ main.m
@@ -0,0 +1,16 @@
+%Plot Earth
+%Plot Mars
+%Launch date, arrival date earth --> mars
+%Launch date, arrival date mars --> earth
+%mission lengths
+%delta V for transfer
+
+G = 6.67430E-11; %m^3/(s^2*kg)
+AU = 149.598E9; %m
+M_s = 1.98847E30;%kg
+M_e = 5.9722E24;%kg
+M_m = 6.4171E23;%kg
+r_e = 1; %AU
+r_M = 1.523679; %AU
+
+
